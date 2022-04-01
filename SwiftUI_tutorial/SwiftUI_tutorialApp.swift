c373923c811ec8c23fd0f8b534288300d8875acf
seongmin221 <seongmin221@naver.com>
2022-04-01 09:22:02 +0900

    Initial Commit

---
+++ SwiftUI_tutorial/SwiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorialApp.swift
+//  SwiftUI_tutorial
+//
+//  Created by 이성민 on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
